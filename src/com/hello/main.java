5ae7c5990ded58c4b87f5d5c90f32d9b80a99026
gef10 <gefinsamuel@gmail.com>
2021-06-23 11:38:18 +0530

    my first commit on the java project

---
+++ src/com/hello/main.java
@@ -0,0 +1,18 @@
+package com.hello;
+import java.util.*;
+public class main {
+	public static void main(String[] args) {
+		   Scanner sc = new Scanner(System.in);
+		   System.out.println("Enter the input value a");
+		   String a = sc.next();
+		   System.out.println("Enter the input value b");
+		   int b = sc.nextInt();
+		   System.out.println("The values are");
+		   System.out.println(a);
+		   System.out.println(b);	
+		
+	}
+
+}
+
+	
